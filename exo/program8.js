b0182d80400fbbab5f5dc32c17d3d672f7d4138f
torrentcome <torrent.come@gmail.com>
2017-04-24 15:28:19 +0200

    step 8 ok

---
+++ exo/program8.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var bl = require('bl');
+
+var url = process.argv[2];
+
+http.get(url, function(res){
+  res.pipe(bl(function(err, data){
+    if(err){
+      return console.error(err);
+    }
+
+    data = data.toString();
+
+    console.log(data.length);
+    console.log(data);
+
+  }));
+  res.on('error', console.error);
+});
