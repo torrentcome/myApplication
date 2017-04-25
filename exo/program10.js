654bf483dcaf637a5362700bbdcdc00245e30e69
torrentcome <torrent.come@gmail.com>
2017-04-25 11:08:40 +0200

    update npm , 10 -> ok

--- exo/program10.js
+++ exo/program10.js
@@ -1,16 +1,30 @@
 var net = require('net');
+var strftime = require('strftime');
+
+// declare
+var port = process.argv[2];
 
 var server = net.createServer(function(socket){
   //begin
-  console.log('client connected');
+  console.log('client connected' + now());
 
   //end
-  c.on('end', function() {
+  socket.on('end', function() {
     console.log('client disconnected');
   });
 
-  c.write('hello\r\n');
-  c.pipe(c);
+  //writing on the server
+  socket.write(strftime(now(), new Date()) + '\n');
+  socket.pipe(socket);
+
+  socket.end();
 
 });
-server.listen(8000);
+
+server.listen(port, function() { //'listening' listener
+  console.log('server bound');
+});
+
+function now(){
+  return strftime('%F %H:%M', new Date());
+}
